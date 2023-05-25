0c59b64ab6aaffe053a2ede032ce7e80e4028e1b
Mato Gudelj <mato.gudelj@tum.de>
2023-05-26 01:47:48 +0200

    Fix freezing strategy for SST.

--- code/freeze_strategies.py
+++ code/freeze_strategies.py
@@ -5,21 +5,24 @@ def freeze_all(model):
 
 
 def unfreeze_last(model, unfreeze_n=1):
-    # Unfreeze the last n layers
-    if "distilbert" in model.name_or_path:
-        # DistilBERT
-        for param in model.distilbert.transformer.layer[-unfreeze_n:].parameters():
-            param.requires_grad = True
-    elif "bert" in model.name_or_path:
-        # BERT
-        for param in model.bert.encoder.layer[-unfreeze_n:].parameters():
-            param.requires_grad = True
+    if unfreeze_n != 0:
+        # Unfreeze the last n layers
+        if "distilbert" in model.name_or_path:
+            # DistilBERT
+            for param in model.distilbert.transformer.layer[-unfreeze_n:].parameters():
+                param.requires_grad = True
+        elif "bert" in model.name_or_path:
+            # BERT
+            for param in model.bert.encoder.layer[-unfreeze_n:].parameters():
+                param.requires_grad = True
 
-    # QA head
+    # QA head if it exists
     if hasattr(model, "qa_outputs"):
         for param in model.qa_outputs.parameters():
             param.requires_grad = True
-
+    elif hasattr(model, "classifier"):
+        for param in model.classifier.parameters():
+            param.requires_grad = True
 
 def all_but_last_n(model, n=1):
     freeze_all(model)
