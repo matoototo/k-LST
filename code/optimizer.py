c8d4c6f828b31072f9014d9203184ed8bb7956e0
Baykam Say <baykam.say@gmail.com>
2023-06-10 00:46:46 +0200

    Add base for lr_distillation

--- code/optimizer.py
+++ code/optimizer.py
@@ -1,3 +1,4 @@
+import math
 import torch.optim as optim
 from transformers import Adafactor
 import re
@@ -6,8 +7,120 @@ import torch
 from torch.optim.lr_scheduler import LambdaLR
 from transformers.optimization import AdafactorSchedule
 
+class Classroom(Adafactor):
+    def __init__(self, *args, training_variables=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.variables = training_variables
+    
+    @staticmethod
+    def _get_lr(param_group, param_state, variables):
+        rel_step_sz = param_group["lr"]
+        if param_group["relative_step"]:
+            min_step = 1e-6 * param_state["step"] if param_group["warmup_init"] else 1e-2
+            rel_step_sz = min(min_step, 1.0 / math.sqrt(param_state["step"]))
+        param_scale = 1.0
+        if param_group["scale_parameter"]:
+            param_scale = max(param_group["eps"][1], param_state["RMS"])
+        return param_scale * rel_step_sz
+    
+    # Default Hugging Face Adafactor implementation with _get_lr modified to use the distillation loss
+    @torch.no_grad()
+    def step(self, closure=None):
+        """
+        Performs a single optimization step
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad
+                if grad.dtype in {torch.float16, torch.bfloat16}:
+                    grad = grad.float()
+                if grad.is_sparse:
+                    raise RuntimeError("Adafactor does not support sparse gradients.")
+
+                state = self.state[p]
+                grad_shape = grad.shape
+
+                factored, use_first_moment = self._get_options(group, grad_shape)
+                # State Initialization
+                if len(state) == 0:
+                    state["step"] = 0
+
+                    if use_first_moment:
+                        # Exponential moving average of gradient values
+                        state["exp_avg"] = torch.zeros_like(grad)
+                    if factored:
+                        state["exp_avg_sq_row"] = torch.zeros(grad_shape[:-1]).to(grad)
+                        state["exp_avg_sq_col"] = torch.zeros(grad_shape[:-2] + grad_shape[-1:]).to(grad)
+                    else:
+                        state["exp_avg_sq"] = torch.zeros_like(grad)
+
+                    state["RMS"] = 0
+                else:
+                    if use_first_moment:
+                        state["exp_avg"] = state["exp_avg"].to(grad)
+                    if factored:
+                        state["exp_avg_sq_row"] = state["exp_avg_sq_row"].to(grad)
+                        state["exp_avg_sq_col"] = state["exp_avg_sq_col"].to(grad)
+                    else:
+                        state["exp_avg_sq"] = state["exp_avg_sq"].to(grad)
+
+                p_data_fp32 = p
+                if p.dtype in {torch.float16, torch.bfloat16}:
+                    p_data_fp32 = p_data_fp32.float()
+
+                state["step"] += 1
+                state["RMS"] = self._rms(p_data_fp32)
+                lr = self._get_lr(group, state, self.variables) # add external variables
+
+                beta2t = 1.0 - math.pow(state["step"], group["decay_rate"])
+                update = (grad**2) + group["eps"][0]
+                if factored:
+                    exp_avg_sq_row = state["exp_avg_sq_row"]
+                    exp_avg_sq_col = state["exp_avg_sq_col"]
+
+                    exp_avg_sq_row.mul_(beta2t).add_(update.mean(dim=-1), alpha=(1.0 - beta2t))
+                    exp_avg_sq_col.mul_(beta2t).add_(update.mean(dim=-2), alpha=(1.0 - beta2t))
+
+                    # Approximation of exponential moving average of square of gradient
+                    update = self._approx_sq_grad(exp_avg_sq_row, exp_avg_sq_col)
+                    update.mul_(grad)
+                else:
+                    exp_avg_sq = state["exp_avg_sq"]
+
+                    exp_avg_sq.mul_(beta2t).add_(update, alpha=(1.0 - beta2t))
+                    update = exp_avg_sq.rsqrt().mul_(grad)
+
+                update.div_((self._rms(update) / group["clip_threshold"]).clamp_(min=1.0))
+                update.mul_(lr)
+
+                if use_first_moment:
+                    exp_avg = state["exp_avg"]
+                    exp_avg.mul_(group["beta1"]).add_(update, alpha=(1 - group["beta1"]))
+                    update = exp_avg
+
+                if group["weight_decay"] != 0:
+                    p_data_fp32.add_(p_data_fp32, alpha=(-group["weight_decay"] * lr))
+
+                p_data_fp32.add_(-update)
+
+                if p.dtype in {torch.float16, torch.bfloat16}:
+                    p.copy_(p_data_fp32)
+
+        return loss
+
 # from https://github.com/r-three/t-few
-def get_optimizer(model, config):
+def get_optimizer(model, config, training_variables):
     """
     Construct optimizer based on config
 
@@ -49,6 +162,16 @@ def get_optimizer(model, config):
             relative_step=False,
             warmup_init=False,
         )
+    elif optim_name.lower() == "classroom":
+        optimizer = Classroom(
+            param_groups,
+            lr=config["lr"],
+            weight_decay=config["weight_decay"],
+            scale_parameter=config["scale_parameter"],
+            relative_step=False,
+            warmup_init=False,
+            training_variables=training_variables,
+        )
     else:
         raise ValueError("Invalid Optimizer name %s" % optim_name)
 
@@ -73,7 +196,7 @@ def get_scheduler(optimizer, config):
         return get_linear_schedule_with_warmup(optimizer, num_warmup_steps, config["num_steps"])
     elif scheduler_name == "cosine_annealing":
         return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, config["num_steps"])
-    elif scheduler_name == "adafactor":
+    elif scheduler_name == "adafactor" or scheduler_name == "classroom":
         return AdafactorSchedule(optimizer, config["lr"])
     else:
         raise ValueError("Invalid Scheduler Name %s" % scheduler_name)
