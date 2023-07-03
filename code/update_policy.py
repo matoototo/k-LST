f35d69efff0fa4eacd89b9e827bbc91ee63c8502
Baykam Say <baykam.say@gmail.com>
2023-07-03 15:40:08 +0200

    Fix tensorboard error

--- code/update_policy.py
+++ code/update_policy.py
@@ -6,22 +6,23 @@ from shutil import rmtree
 
 
 class UpdatePolicyCallback(TensorBoardCallback):
-    deleted_logs = False
 
     def on_log(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, logs=None, **kwargs):
         # Log peak allocated memory in MiB and reset it
         logs["memory"] = memory_stats()["allocated_bytes.all.peak"] / 1048576
         reset_peak_memory_stats()
 
-        # Delete unnecessary tensorboard files and folders. Doing this once on the first training log is enough.
-        if not self.deleted_logs and state.global_step > 0:
-            self.deleted_logs = True
-            for root, dirs, files in os.walk(args.logging_dir):
-                for d in dirs:
-                    rmtree(f"{args.logging_dir}/{d}")
-                if len(files) > 0:
-                    files.pop()
-                    for file in files:
-                        os.remove(f"{args.logging_dir}/{file}")
-
         super().on_log(args, state, control, logs=logs, **kwargs)
+    
+    def on_train_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+
+        # Delete unnecessary tensorboard files and folders
+        for root, dirs, files in os.walk(args.logging_dir):
+            for d in dirs:
+                rmtree(f"{args.logging_dir}/{d}")
+            if len(files) > 0:
+                files.pop()
+                for file in files:
+                    os.remove(f"{args.logging_dir}/{file}")
+
+        super().on_train_end(args, state, control, **kwargs)
